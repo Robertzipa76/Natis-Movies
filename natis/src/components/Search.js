44f0b5a88d8be62dcc90d88618f2ab2c955e4142
Robert Andersson <biggbucks45@gmail.com>
2022-10-21 16:05:53 +0000

    code in the components

--- natis/src/components/Search.js
+++ natis/src/components/Search.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Header = (props) => {
-    return (
-        <header className="App-header">
-            <h2>{props.text}</h2>
-        </header>
+
+const Search = (props) => {
+  const [searchValue, setSearchValue] = useState("");
+  
+  const handleSearchInputChanges = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const resetInputField = () => {
+    setSearchValue("")
+  }
+
+  const callSearchFunction = (e) => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInputField();
+  }
+
+  return (
+      <form className="search">
+        <input
+          value={searchValue}
+          onChange={handleSearchInputChanges}
+          type="text"
+        />
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+      </form>
     );
-};
+}
 
-export default Header;
+
+export default Search;
